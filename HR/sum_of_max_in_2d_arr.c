b688b13876f0f4d21b3f68ad8b3e68d877c5469a
Anil Kumar <janil443@gmail.com>
2019-04-18 21:53:25 +0530

    sum_of_max_in_2d_arr.c done

--- HR/sum_of_max_in_2d_arr.c
+++ HR/sum_of_max_in_2d_arr.c
@@ -1,18 +1,131 @@
+#include <assert.h>
+#include <limits.h>
+#include <math.h>
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+char* readline();
+char** split_string(char*);
+
+// Complete the hourglassSum function below.
 int hourglassSum(int arr_rows, int arr_columns, int** arr) {
-    int max =0, c,n,r,sum;
-    n = (arr_rows - 2) * (arr_columns -2);
+  int max =0;
+  int sum =0;
+  int r=0;
+   int c =0;
+    //int c_limit = ((arr_columns -2) < 6 )? (arr_columns - 2):6;
+    //int r_limit = ((arr_rows -2) < 6 )? (arr_rows - 2):6;
+    int c_limit = 6;
+    int r_limit = 6;
+    for (c =0; c < c_limit ; c++){
 
-    for (c = 0; c < (arr_columns -2) ; c++ )
-    {
+        for(r =0; r < r_limit; r++){
 
-        for (r = 0; r < (arr_rows - 2) ; r++){
+          sum = (arr[r][c]+arr[r][c+1]+arr[r][c+2]+arr[r+1][c+1]+arr[r+2][c]+arr[r+2][c+1]+arr[r+2][c+2]);
+	  	printf("arr[%d][%d]+arr[%d][%d]+arr[%d][%d]+arr[%d][%d]+arr[%d][%d]+arr[%d][%d]+arr[%d][%d] \n",
+		(r,c,r,c+1,r,c+2,r+1,c+1,r+2,c,r+2,c+1,r+2,c+2));
+		if(c == 0)
+		max = sum;
 
-            sum = (arr[r][c]+arr[r][c+1]+arr[r][c+2]+ arr[r+1][c+1]+arr[r+2][c]+arr[r+2][c+1]+arr[r+2][c+2]);
-
-            if (sum > max)
-                max = sum;
+            if(sum >= max)
+              max = sum;
 
         }
+
+
     }
-    return max;
+
+
+return max;
+
+}
+
+int main()
+{
+    FILE* fptr = fopen(getenv("OUTPUT_PATH"), "w");
+
+    int** arr = malloc(6 * sizeof(int*));
+
+    for (int i = 0; i < 6; i++) {
+        *(arr + i) = malloc(6 * (sizeof(int)));
+
+        char** arr_item_temp = split_string(readline());
+
+        for (int j = 0; j < 6; j++) {
+            char* arr_item_endptr;
+            char* arr_item_str = *(arr_item_temp + j);
+            int arr_item = strtol(arr_item_str, &arr_item_endptr, 10);
+
+            if (arr_item_endptr == arr_item_str || *arr_item_endptr != '\0') { exit(EXIT_FAILURE); }
+
+            *(*(arr + i) + j) = arr_item;
+        }
+    }
+
+    int arr_rows = 6;
+    int arr_columns = 6;
+
+    int result = hourglassSum(arr_rows, arr_columns, arr);
+
+    fprintf(fptr, "%d\n", result);
+
+    fclose(fptr);
+
+    return 0;
+}
+
+char* readline() {
+    size_t alloc_length = 1024;
+    size_t data_length = 0;
+    char* data = malloc(alloc_length);
+
+    while (true) {
+        char* cursor = data + data_length;
+        char* line = fgets(cursor, alloc_length - data_length, stdin);
+
+        if (!line) { break; }
+
+        data_length += strlen(cursor);
+
+        if (data_length < alloc_length - 1 || data[data_length - 1] == '\n') { break; }
+
+        size_t new_length = alloc_length << 1;
+        data = realloc(data, new_length);
+
+        if (!data) { break; }
+
+        alloc_length = new_length;
+    }
+
+    if (data[data_length - 1] == '\n') {
+        data[data_length - 1] = '\0';
+    }
+
+    data = realloc(data, data_length);
+
+    return data;
+}
+
+char** split_string(char* str) {
+    char** splits = NULL;
+    char* token = strtok(str, " ");
+
+    int spaces = 0;
+
+    while (token) {
+        splits = realloc(splits, sizeof(char*) * ++spaces);
+        if (!splits) {
+            return splits;
+        }
+
+        splits[spaces - 1] = token;
+
+        token = strtok(NULL, " ");
+    }
+
+    return splits;
 }
